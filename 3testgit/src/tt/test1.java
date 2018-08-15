7bd8f6694c40cd1679dc168fd2a2e80f5ceca9a8
cm <1010712426@qq.com>
2018-08-15 10:51:36 +0800

    空间和规范大三

--- 3testgit/src/tt/test1.java
+++ 3testgit/src/tt/test1.java
@@ -3,7 +3,7 @@ package tt;
 public class test1 {
 
 	public static void main(String[] args) {
-		System.out.println("第二次修改");
+		System.out.println("第san次修改");
 		System.out.println("asdfghjkl");
 
 	}
